69b8706064675228b87f7131f37e9ee352c4c4b0
LMC <101943576+april808@users.noreply.github.com>
2022-12-31 13:30:43 +0800

    整理內容

--- src/script.js
+++ src/script.js
@@ -10,22 +10,12 @@ const toggleblack = (chkel) => {
     }
     console.log(chkel.checked)
 };
-
-// window.addEventListener("DOMContentLoaded", function() {
-//   document.querySelector("#addcss").addEventListener('change', function(el) {
-//   console.log(el.target.checked);
-//   localStorage.setItem('input_checked', el.target.checked );
-//   if ( localStorage.getItem('input_checked') !== null ) {
-//       document.querySelector('#chk').checked = localStorage.getItem('input_checked') === 'true';
-//     }
-//   }
-// });
   
 /*進場動態*/
 
 setTimeout(function () {
-    $(".top-nav").removeClass("pre-rehearsal");
+    document.querySelector(".top-nav").classList.remove("pre-rehearsal");
 }, 800);/*拿掉隱藏用CSS*/
 setTimeout(function () {
-    $(".LinkList").removeClass("start-ani");
+    document.querySelector(".LinkList").classList.remove("start-ani");
 }, 7000);/*拿掉延遲用CSS*/
